34a0f36e2380b5008a181992b7a9dfb9e156e04a
Steve Sewell <steve@builder.io>
2022-05-15 13:15:57 -0700

    builder text

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,11 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography")({
+      className: "builder-text",
+    }),
+  ],
+};
